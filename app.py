931f7eacf9ae7b2230c7d296b5183905bad0bc55
ItsWeslie <itssamwesliehere@gmail.com>
2025-03-17 19:37:51 +0530

    app.py files changed

--- app.py
+++ app.py
@@ -315,26 +315,21 @@ def create_route():
     start_location = request.form["start_location"]
     end_location = request.form["end_location"]
     stops = request.form["stops"]
-<<<<<<< HEAD
-    fees =  request.form["fees"]
-=======
+
+
     fees = request.form["fees"]
->>>>>>> 116759c701470f81060a158ad313de44aa739037
+
 
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
 
     # Insert data into the database
     cursor.execute("""
-<<<<<<< HEAD
+
         INSERT INTO bus_routes (route_name, start_location, end_location, stops, created_at, fees)
         VALUES (%s, %s, %s, %s, NOW(), %s)
     """, (route_name, start_location, end_location, stops, fees))
-=======
-        INSERT INTO bus_routes (route_name, start_location, end_location, stops, created_at,fees)
-        VALUES (%s, %s, %s, %s,NOW(), %s)
-    """, (route_name, start_location, end_location, stops,fees))
->>>>>>> 116759c701470f81060a158ad313de44aa739037
+
 
     connection.commit()
     cursor.close()
